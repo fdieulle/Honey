87a92c8c312f5287cfc4e8a55d90a0f5fa2f146d
fdieulle <fabiendieull@hotmail.fr>
2022-03-31 20:12:19 -0400

    setup AntDesign and the swagger

--- Dojo/Startup.cs
+++ Dojo/Startup.cs
@@ -1,8 +1,10 @@
+using Dojo.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace Dojo
 {
@@ -21,6 +23,13 @@ namespace Dojo
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
+            services.AddAntDesign();
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Shogun", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -29,6 +38,8 @@ namespace Dojo
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Shogun v1"));
             }
             else
             {
