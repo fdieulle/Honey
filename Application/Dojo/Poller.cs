6473bf3ddd9cc6c349c7ad76e89d97e57b4ee7da
fdieulle <fabiendieull@hotmail.fr>
2022-12-10 22:29:57 -0500

    Add a service which is responsible to poll ninja data

---
+++ Application/Dojo/Poller.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Application.Dojo
+{
+    public class Poller : IDisposable
+    {
+        private readonly ITimer _timer;
+        private readonly Dojo _dojo;
+        private readonly QueueProvider _queues;
+
+        public Poller(ITimer timer, Dojo dojo, QueueProvider queues)
+        {
+            _timer = timer;
+            _dojo = dojo;
+            _queues = queues;
+        }
+        public void Start() => _timer.Updated += Refresh;
+
+        private void Refresh()
+        {
+            _dojo.Refresh();
+            _queues.Refresh();
+        }
+
+        public void Dispose() => _timer.Updated -= Refresh;
+    }
+}
